dfea9f329cf43a8d51843dd575cb800f128ce2cc
Alicia Szerenyi <alicia.szerenyi@quanam.com>
2020-04-16 20:06:23 -0300

    WIP magna_factura_electronica

--- magna_factura_electronica/models/taxes.py
+++ magna_factura_electronica/models/taxes.py
@@ -6,6 +6,6 @@ from odoo import api, fields, models
 class account_tax(models.Model):
     _inherit = "account.tax"
 
-    fe_indicador_facturacion = fields.Char('Código de impuesto de la DGI', size=2),
+    fe_indicador_facturacion = fields.Char('Código de impuesto de la DGI', size=2)
 
 
