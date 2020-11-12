5961541e3c3c11cb53262e20b9f111aee31b3dae
Alicia Szerenyi <alicia.szerenyi@quanam.com>
2020-11-12 13:13:49 -0300

    Cambio de Serie y Codigo que se envia a Proinfo

--- magna_factura_electronica/models/account_move.py
+++ magna_factura_electronica/models/account_move.py
@@ -214,8 +214,8 @@ class AccountMove(models.Model):
 
             # ADICIONAL
             options._adicionalTipoDocumentoId = tipo_CFE
-            options._adicionalDocComSerie = rec.name[:-4]
-            options._adicionalDocComCodigo = rec.name[-4:]
+            options._adicionalDocComSerie = 'v13id' + str(rec.id)
+            options._adicionalDocComCodigo = rec.name
             options._adicionalSucursalId = rec.company_id.fe_codigo_principal_sucursal
             options._adicionalAdenda = ''
             options._adicionalCorreoReceptor = ''
