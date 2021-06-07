8f8208b25ef98b5da122b39c5350c0d2e065c51f
Alicia Szerenyi <alicia.szerenyi@quanam.com>
2021-06-07 17:10:50 -0300

    magna_discount_helper

--- magna_discount_helper/models/account_move.py
+++ magna_discount_helper/models/account_move.py
@@ -10,8 +10,7 @@ class AccountMove(models.Model):
 
     my_discount = fields.Float(string='Descuento a aplicar')
 
-    @api.onchange('my_discount')
-    def apply_discount(self):
+    def button_apply_discount(self):
         for rec in self:
             for line in rec.invoice_line_ids:
                 line.discount = rec.my_discount
--- magna_discount_helper/models/sale_order.py
+++ magna_discount_helper/models/sale_order.py
@@ -8,9 +8,9 @@ class SaleOrder(models.Model):
 
     my_discount = fields.Float(string='Descuento a aplicar')
 
-    @api.onchange('my_discount')
-    def apply_discount(self):
+    def button_apply_discount(self):
         for rec in self:
             for line in rec.order_line:
                 line.discount = rec.my_discount
+
 
