49c69784ee3551503a0187f26feab839239ff034
Alicia Szerenyi <alicia.szerenyi@quanam.com>
2021-06-07 16:39:03 -0300

    magna_discount_helper

--- magna_discount_helper/models/sale_order.py
+++ magna_discount_helper/models/sale_order.py
@@ -6,11 +6,17 @@ from odoo import api, models, fields
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    discount = fields.Float(string='Descuento a aplicar')
+    my_discount = fields.Float(string='Descuento a aplicar')
 
-    @api.onchange('discount')
-    def apply_discount(self):
+    def button_apply_discount(self):
         for rec in self:
             for line in rec.order_line:
-                line.discount = rec.discount
+                line.discount = rec.my_discount
+            rec.my_discount = 0
+
+    # @api.onchange('discount')
+    # def apply_discount(self):
+    #     for rec in self:
+    #         for line in rec.order_line:
+    #             line.discount = rec.discount
 
