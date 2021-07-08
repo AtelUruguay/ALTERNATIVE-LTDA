a2b729761fc0304e9352b3aa1413f9ee66167d2e
Alicia Szerenyi <alicia.szerenyi@quanam.com>
2021-07-08 11:23:54 -0300

    magna_discount_helper

--- magna_discount_helper/models/account_move.py
+++ magna_discount_helper/models/account_move.py
@@ -13,8 +13,10 @@ class AccountMove(models.Model):
     @api.onchange('my_discount')
     def onchange_my_discount(self):
         for rec in self:
-            if rec.my_discount > 0:
-                for line in rec.invoice_line_ids:
-                    line.discount = rec.my_discount
-                    # line._onchange_price_subtotal()
-            return {'value': {'my_discount': 0}}
+            for line in rec.invoice_line_ids:
+                line.discount = rec.my_discount
+                # line._onchange_price_subtotal()
+                # self._recompute_dynamic_lines(recompute_all_taxes=True)
+            rec.line_ids._onchange_price_subtotal()
+            rec._recompute_dynamic_lines(recompute_all_taxes=True)
+        return {'value': {'my_discount': 0}}
