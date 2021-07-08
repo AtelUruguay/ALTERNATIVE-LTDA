be69e097f51066a8f4ef4a48749b0fc4d4938c8d
Alicia Szerenyi <alicia.szerenyi@quanam.com>
2021-07-08 13:44:57 -0300

    magna_discount_helper

---
+++ magna_discount_helper/models/account_move.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+import logging
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    my_discount = fields.Float(string='Descuento a aplicar')
+
+    @api.onchange('my_discount')
+    def onchange_my_discount(self):
+        for line in self.invoice_line_ids:
+            line.discount = self.my_discount
+        self.invoice_line_ids._onchange_price_subtotal()
+        self._recompute_dynamic_lines(recompute_all_taxes=True)
+
---
+++ magna_discount_helper/models/sale_order.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    my_discount = fields.Float(string='Descuento a aplicar')
+
+    @api.onchange('my_discount')
+    def onchange_my_discount(self):
+        for line in self.order_line:
+            line.discount = self.my_discount
+
+
