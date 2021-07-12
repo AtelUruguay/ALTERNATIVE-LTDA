5810343a7e28f73cb95ba7ed3a6fdf27198ca316
rpelaez <rpelaezg87@gmail.com>
2021-07-12 14:30:40 -0300

    [FIX] (sale_discount_limit) Corrección en función _check_discount de sale.order.line ps07:8707

--- sale_discount_limit/models/sale_order.py
+++ sale_discount_limit/models/sale_order.py
@@ -11,13 +11,14 @@ class SaleOrderLine(models.Model):
 
     @api.constrains('discount')
     def _check_discount(self):
-        if self.discount:
-            discount_amt = False
-            for group in self.env['sales.discount.limit'].search([]):
-                if self._uid in group.group_id.users.ids and not discount_amt:
-                    discount_amt = group.discount
-                    break
-            if discount_amt and self.discount > discount_amt:
-                raise UserError(
-                    _('You are not eligible to give discount '
-                      'more than %s %%.' % group.discount))
+        for rec in self:
+            if rec.discount:
+                discount_amt = False
+                for group in self.env['sales.discount.limit'].search([]):
+                    if self._uid in group.group_id.users.ids and not discount_amt:
+                        discount_amt = group.discount
+                        break
+                if discount_amt and rec.discount > discount_amt:
+                    raise UserError(
+                        _('You are not eligible to give discount '
+                          'more than %s %%.' % group.discount))
