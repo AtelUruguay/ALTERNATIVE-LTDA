9cd52627ee8428c40a7a1187566b3fe0617059af
AtelUruguay <rpelaezg87@gmail.com>
2022-01-17 13:19:02 -0300

    [ADD]client_sale_report Agregando traducciones, campo date_order ps07: 9587

--- client_sale_report/models/sale_order_line.py
+++ client_sale_report/models/sale_order_line.py
@@ -5,4 +5,5 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     partner_id_email_related = fields.Char(related='order_partner_id.email')
+    date_order = fields.Datetime(related='order_id.date_order')
 
