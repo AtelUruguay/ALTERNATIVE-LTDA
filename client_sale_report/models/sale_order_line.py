f0507406b541e103cbf29de2d52b21e3fa5d475c
AtelUruguay <rpelaezg87@gmail.com>
2022-03-17 12:09:27 -0300

    [ADD]client_sale_report
    Agregando product_uom_change en sale_order_line.py para corregir el calculo del price_unit (Eliminar en futuras actualizaciones)

--- client_sale_report/models/sale_order_line.py
+++ client_sale_report/models/sale_order_line.py
@@ -68,3 +68,29 @@ class SaleOrderLine(models.Model):
                 self.product_id = False
 
         return result
+
+    # Todo eliminar luego que odoo sh contentga el commit 2e23c85a879e5c5c2cca6d1e3d4cdb94d47f2cba
+    @api.onchange('product_uom', 'product_uom_qty')
+    def product_uom_change(self):
+        if not self.product_uom or not self.product_id:
+            self.price_unit = 0.0
+            return
+        if self.order_id.pricelist_id and self.order_id.partner_id:
+            product = self.product_id.with_context(
+                lang=self.order_id.partner_id.lang,
+                partner=self.order_id.partner_id,
+                quantity=self.product_uom_qty,
+                date=self.order_id.date_order,
+                pricelist=self.order_id.pricelist_id.id,
+                uom=self.product_uom.id,
+                fiscal_position=self.env.context.get('fiscal_position')
+            )
+            self.price_unit = product._get_tax_included_unit_price(
+                self.company_id or self.order_id.company_id,
+                self.order_id.currency_id,
+                self.order_id.date_order,
+                'sale',
+                fiscal_position=self.order_id.fiscal_position_id,
+                product_price_unit=self._get_display_price(product),
+                product_currency=self.order_id.currency_id
+            )
