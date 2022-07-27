85a01f9241ad94208017958f22478406a345ef1e
AtelUruguay <rpelaezg87@gmail.com>
2022-07-27 12:28:18 -0300

    [ADD]sales_team_custom Agregando default al campo is_group_sale_salesman para que no espere al compute y se haga readonly segun corresponda al agregar una nueva línea ps07: 10125

--- sales_team_custom/models/sale_order.py
+++ sales_team_custom/models/sale_order.py
@@ -6,9 +6,12 @@ from odoo import fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    is_group_sale_salesman = fields.Boolean(compute='_compute_is_group_sale_salesman')
+    is_group_sale_salesman = fields.Boolean(compute='_compute_is_group_sale_salesman',
+                                            default=lambda self: self._compute_is_group_sale_salesman(is_default=True))
 
-    def _compute_is_group_sale_salesman(self):
+    def _compute_is_group_sale_salesman(self, is_default=False):
+        result = self.user_has_groups('sales_team.group_sale_salesman,sales_team.group_sale_salesman_all_leads')
+        if is_default:
+            return result
         for rec in self:
-            rec.is_group_sale_salesman = self.user_has_groups(
-                'sales_team.group_sale_salesman,sales_team.group_sale_salesman_all_leads')
+            rec.is_group_sale_salesman = result
