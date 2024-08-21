1735b2f28f3f2490d7caaf048dc4097906f8108b
Rafael Aguero Baquero <raguero@quanam.com>
2024-08-20 21:32:45 -0300

    [MIG] Cleaning scripts using openupgrade tool

--- account_multicurrency_revaluation/__manifest__.py
+++ account_multicurrency_revaluation/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Skeleton of Multicurrency revaluation",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Finance",
     "summary": "Skeleton addon of Manage revaluation for multicurrency environment",
     "author": "Skeleton addon of Camptocamp, Odoo Community Association (OCA)",
