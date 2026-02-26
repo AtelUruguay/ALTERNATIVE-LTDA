8cc50611aef3c8c196fcaf272c113f925dd69b30
rpelaezg87 <rpelaezg87@gmail.com>
2026-02-26 09:19:41 -0300

    Facturación electrónica: modificaciones solicitadas por DGI ps07: 15842

--- magna_factura_electronica/models/fe_xml_factory.py
+++ magna_factura_electronica/models/fe_xml_factory.py
@@ -223,11 +223,11 @@ class CfeFactory():
             
             if self.opt._referenciaMntCFEref:
                 # Entra si es una NC con referencia a CFE
-                self._set_fe_node_data(doc, SubZonaReferencia, 'FEREFMntCFE',
+                self._set_fe_node_data(doc, SubZonaReferencia, 'FEREFMntCFEref',
                                        str(self.opt._referenciaMntCFEref))
-                self._set_fe_node_data(doc, SubZonaReferencia, 'FEREFTpoMoneda',
+                self._set_fe_node_data(doc, SubZonaReferencia, 'FEREFTpoMonedaRef',
                                        str(self.opt._referenciaTpoMonedaRef))
-                self._set_fe_node_data(doc, SubZonaReferencia, 'FEREFTpoCambio',
+                self._set_fe_node_data(doc, SubZonaReferencia, 'FEREFTpoCambioRef',
                                        str("{0:.3f}".format(self.opt._referenciaTpoCambioRef)))
 
 
