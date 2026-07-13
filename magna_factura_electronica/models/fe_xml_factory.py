41439f60c4f809c32f580cf512fb567e131ae925
rpelaezg87 <rpelaezg87@gmail.com>
2026-07-13 13:50:12 -0300

    [FIX] Facturación electrónica En las notas de crédito siempre agregamos la sección de referencias además cambiamos el indicador global para que sea igual a uno si no si la nota de crédito no tiene factura asociada agregamos también que enviara la fecha de la factura de referencia

--- magna_factura_electronica/models/fe_xml_factory.py
+++ magna_factura_electronica/models/fe_xml_factory.py
@@ -235,7 +235,7 @@ class CfeFactory():
                                         str("{0:.3f}".format(self.opt._referenciaTpoCambioRef)))
             else:
                 # Entra si es una NC con referencia a CFE sin monto de referencia (caso NC con referencia a CFE sin monto pero con indicador global 2 o 3)
-                self._set_fe_node_data(doc, SubZonaReferencia, 'FEREFIndicadorGlobal', str(self.opt._referenciaIndicadorGlobal))
+                self._set_fe_node_data(doc, SubZonaReferencia, 'FEREFIndGlobal', str(self.opt._referenciaIndicadorGlobal))
                 self._set_fe_node_data(doc, SubZonaReferencia, 'FEREFRazon', self.opt._referenciaRazon or 'N/A')
 
 
