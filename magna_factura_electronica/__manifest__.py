6d63d8a7b20bdfc3890a70ad0c8de406c5f8a433
rpelaezg87 <rpelaezg87@gmail.com>
2026-07-23 16:42:44 -0300

    [FIX] Add currency rate fields and logic to account.move model and views

--- magna_factura_electronica/__manifest__.py
+++ magna_factura_electronica/__manifest__.py
@@ -8,7 +8,7 @@
     'category': 'Localizacion',
     'author': 'Quanam',
     'website': 'https://www.quanam.com',
-    'depends' : ['base','account','base_currency_inverse_rate','web'],
+    'depends' : ['base','account','web'],
     'data': [
         'security/ir.model.access.csv',
         'data/fe_data.xml',
